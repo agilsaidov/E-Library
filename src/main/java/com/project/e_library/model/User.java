3aac5b4335640a088cd0fb647d768c7c1f64d027
Aqil Səidov <aqilasa638@gmail.com>
2025-09-14 03:48:19 +0400

    User Entity class added

---
+++ src/main/java/com/project/e_library/model/User.java
@@ -0,0 +1,58 @@
+package com.project.e_library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private String auth0_id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birth_date")
+    private Date birthday;
+
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
+    private String gender;
+
+    @Column(name = "picture_url")
+    private String pictureUrl;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private String role;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
